cd7aa4c5edefa3aaf65f097d282d620d5500afd6
VishDroid-dev <vk2345732@gmail.com>
2021-05-17 22:24:01 +0530

    added a template

--- viper/viper/settings.py
+++ viper/viper/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'viper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,10 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'cdn/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_project')]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", 'static_root')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'cdn/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", 'media_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
